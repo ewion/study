5895e5d6d0e21a9b8c0966210b88c805c51f7dec
ewion <dnznjt05@gmail.com>
2023-08-16 20:50:47 +0900

    230813

---
+++ src/main/java/CodingTest14.java
@@ -0,0 +1,8 @@
+public class CodingTest14 {
+    public static void main(String[] args) {
+        String answer = "";
+        System.out.println(answer);
+
+        }
+
+}
---
+++ src/main/java/CodingTest15.java
@@ -0,0 +1,9 @@
+public class CodingTest15 {
+    public static void main(String[] args) {
+        //옹알이1
+        String answer = "";
+        System.out.println(answer);
+
+        }
+
+}
